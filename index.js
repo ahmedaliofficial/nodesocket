841867223d86e977ec8dda77d50a1a58bd7b6549
Pakistan Developers <85020041+pakistandeveloper@users.noreply.github.com>
2023-01-29 16:58:44 +0500

    asdas
    dasd

--- index.js
+++ index.js
@@ -29,7 +29,8 @@ io.on('connection', (socket) => {
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-server.listen(PORT, () => {
+
+app.listen(PORT, () => {
     console.log('Server is running');
 });
 
