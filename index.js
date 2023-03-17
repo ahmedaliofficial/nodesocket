c6f7263f4f9f49ebdf85de325d04d1ac60ff10aa
Pakistan Developers <85020041+pakistandeveloper@users.noreply.github.com>
2023-03-17 21:55:36 +0500

    update cor cyclic
    update cor cyclic

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express')
 
 const app = express();
-
-
+const { instrument } = require("@socket.io/admin-ui");
+// const path = require('path')
+// const fs = require('fs')
 const server = require('http').createServer(app);
-const PORT =  5000;
+// const server = require('https').createServer({
+//     key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+//     cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
+// }, app);
+const PORT = 3000;
 
 const io = require('socket.io')(server, {
-    cors: "*"
+    // cors: "*",
+    cors: {
+        origin: ["https://admin.socket.io", "http://localhost:3000", "https://softmeck-renew.vercel.app"],
+        credentials: true
+    }
 });
 
 var users = [];
@@ -15,7 +24,7 @@ var users = [];
 io.on('connection', (socket) => {
     // console.log(socket.handshake.query['user']);
     console.log('connect')
-    
+
     socket.on('chattotserver', (message) => {
         // message = JSON.parse(message)
         // io.sockets.emit('sendChatToClient', message);
@@ -24,7 +33,7 @@ io.on('connection', (socket) => {
     socket.on('connecttoserver', (data) => {
         // io.sockets.emit('sendChatToClient', message);
         users[parseInt(data.userid)] = socket.id
-        console.log(users[parseInt(data.userid)],"connected",data,users)
+        console.log(users[parseInt(data.userid)], "connected", data, users)
     });
 
 
@@ -35,9 +44,17 @@ io.on('connection', (socket) => {
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+instrument(io, {
+    auth: {
+        type: "basic",
+        username: "admin",
+        password: "$2a$12$gWoP0/1vZ.zqs.52wpYT..5.kxpWwxJV.3hXKGB8XX43j3mx6H076" // "changeit" encrypted with bcrypt
 
+    },
+    mode: "development",
+});
 server.listen(PORT, () => {
-    console.log('Server is running');
+    console.log('Server is running', PORT);
 });
 
 
